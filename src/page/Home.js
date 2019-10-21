a917519aa89a6739e336b7c0fe5ffd074d0b9a1f
chanmi <cksal4785911@gmail.com>
2019-10-21 14:23:47 +0900

    MainPageUi
    Create MainPageUi

---
+++ src/page/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import HeaderMenu from '../component/HeaderMenu';
+import Mainbanner from '../component/Mainbanner';
+
+const Home = () => {
+    return (
+        <div>
+            <HeaderMenu/>
+            <Mainbanner/>
+        </div>
+    );
+};
+
+export default Home;
