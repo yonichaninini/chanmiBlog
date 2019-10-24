3da22ab8d14ce626aa531d533d79df4e092769a7
chanmi <cksal4785911@gmail.com>
2019-10-24 15:20:53 +0900

    add  wrapperComponent
    card component를 감싸는 wrapperComponent를 생성하였다

--- src/page/Home.js
+++ src/page/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import HeaderMenu from '../component/HeaderMenu';
 import Mainbanner from '../component/Mainbanner';
+import IntroduceCardWrapper from '../component/IntroduceCardWrapper';
 
 const Home = () => {
     return (
         <div>
             <HeaderMenu/>
             <Mainbanner/>
+            <IntroduceCardWrapper/>
         </div>
     );
 };
